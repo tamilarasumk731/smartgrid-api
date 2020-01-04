ecc3e8e6a20b2494c200db5bd735d31a24be471d
tamilarasumk731 <tamilarasu.mk731@gmail.com>
2020-01-04 23:36:03 +0530

    Order pickup api added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :orders, only: [] do
+    collection do
+      put '/pickedup', to: "orders#order_picked_up"
+    end
+  end
 end
