482c70552fd3fb1ef3238b979e8ecced47aeb225
tamilarasumk731 <tamilarasu.mk731@gmail.com>
2020-01-04 19:41:59 +0530

    Added rails admin

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module SmartgridApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Session::CookieStore, {:key=>"_smartgrid_api_session"}
   end
 end
